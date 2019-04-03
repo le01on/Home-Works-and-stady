300e03dbc990edc9d8a90e1431245e3f247c6eb8
Igor Lem <le01n@ukr.net>
2019-04-03 10:51:20 +0300

    remake

--- lesson6/task11.1.php
+++ lesson6/task11.1.php
@@ -1,1 +1,15 @@
 <?php
+
+function revers($n)
+{
+    $i = strlen($n);
+    if ($i > 0) {
+        echo substr($n, $i - 1, 1) . " ";
+        $n = substr($n, 0, -1);
+    }
+    if (!empty($n)) {
+        revers($n);
+    }
+}
+
+revers(8795);
