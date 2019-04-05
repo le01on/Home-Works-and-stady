b340bf71e4eb7152cd0b793fbb58e0402789d54e
Igor Lem <le01n@ukr.net>
2019-04-05 13:41:51 +0300

    update

--- lesson7/task13.php
+++ lesson7/task13.php
@@ -1,1 +1,18 @@
 <?php
+if (!isset($_COOKIE['counter'])) {
+    $_COOKIE['counter'] = 0;
+} else
+    $_COOKIE['counter']++;
+setcookie('counter', $_COOKIE['counter']);
+
+if (isset($_POST['name'])) {
+    $_COOKIE['name'] = $_POST['name'];
+}
+   echo "Добро пожаловать " . $_COOKIE['name'] . "<br>";
+
+echo "Посищений  страницы: " . $_COOKIE['counter'] . "." . "<br>";
+?>
+<form action="" method="post">
+<input name="name" value="<?php if (isset($_POST['name'])) echo $_POST['name']?>">
+<input type="submit" value="Your Nik">
+</form>
